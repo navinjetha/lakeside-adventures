5a6e27e726e3b4218941ae93790db2f806421337
Scott Luxton <78744890+ScottLuxton@users.noreply.github.com>
2021-05-25 14:34:17 -0600

    Scott edit on rentals and lessons

--- client/src/components/MyPricingTable.js
+++ client/src/components/MyPricingTable.js
@@ -11,21 +11,39 @@ function MyPricingTable() {
           <div className ='pricingDetail'>
             <PricingTable  highlightColor='#1976D2'>
               <PricingSlot onClick={submit} buttonText='Book Now' title='Lessons' >
-                <PricingDetail> $xx / Hour Private </PricingDetail>
-                <PricingDetail> $xx /Hour Group </PricingDetail>
-                <PricingDetail> up to three people</PricingDetail>
+                <PricingDetail> Wake boarding / surfing </PricingDetail>
+                <PricingDetail> Water skiing </PricingDetail>
+                <PricingDetail> Up to three people</PricingDetail>
                 <PricingDetail> Custom and full day rates available</PricingDetail>
               </PricingSlot>
               <PricingSlot  onClick={submit} buttonText='Book Now' title='Jet Ski Rentals' >
                 <PricingDetail> Two riders per machine max </PricingDetail>
-                <PricingDetail> $xx 45 mins </PricingDetail>
-                <PricingDetail> $xx 90 mins</PricingDetail>
+                <PricingDetail> Our most high speed adventure </PricingDetail>
+                <PricingDetail> Life jackets provided </PricingDetail>
                 <PricingDetail> Custom and full day rates available</PricingDetail>
               </PricingSlot>
-              <PricingSlot  onClick={submit} buttonText='Book Now' title='Lake Toys' >
-                <PricingDetail> Inflatables $xx / hour</PricingDetail>
-                <PricingDetail> Kayaks $xx / hour </PricingDetail>
-                <PricingDetail> Paddleboards $/xx / hour </PricingDetail>
+              <PricingSlot  onClick={submit} buttonText='Book Now' title='Paddleboards' >
+                <PricingDetail> Great exercise</PricingDetail>
+                <PricingDetail> Paddles and life jackets included </PricingDetail>
+                <PricingDetail> We provide a brief introduction </PricingDetail>
+                <PricingDetail> Custom and full day rates available</PricingDetail>
+              </PricingSlot>
+              <PricingSlot  onClick={submit} buttonText='Book Now' title='Kayaks' >
+                <PricingDetail> One and Two person available</PricingDetail>
+                <PricingDetail> Paddles and life jackets included </PricingDetail>
+                <PricingDetail> Great for fishing</PricingDetail>
+                <PricingDetail> Custom and full day rates available</PricingDetail>
+              </PricingSlot>
+              <PricingSlot  onClick={submit} buttonText='Book Now' title='Inflatables' >
+                <PricingDetail> Bannana Boats</PricingDetail>
+                <PricingDetail> Tubing </PricingDetail>
+                <PricingDetail> Rafts</PricingDetail>
+                <PricingDetail> Custom and full day rates available</PricingDetail>
+              </PricingSlot>
+              <PricingSlot  onClick={submit} buttonText='Book Now' title='High Adrenaline Sports' >
+                <PricingDetail> Water skiing</PricingDetail>
+                <PricingDetail> Wake boarding </PricingDetail>
+                <PricingDetail> Wake surfing </PricingDetail>
                 <PricingDetail> Custom and full day rates available</PricingDetail>
               </PricingSlot>
             </PricingTable>
